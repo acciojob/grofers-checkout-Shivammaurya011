68649d392a87b2bd3a17e699b223b39dad678022
Shivammaurya011 <shivammaurya011@gmail.com>
2023-02-27 16:42:08 +0530

    Submited

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    var table = document.querySelector('tbody');
+    let subTotal = Array.from(table.rows).slice(1).reduce((total, row) => {
+        return total + parseFloat(row.cells[1].innerHTML);
+      }, 0);
+    console.log(subTotal)
+  document.getElementById('tp').innerText = `Total Price: ${subTotal}`;
+
 };
 
 getSumBtn.addEventListener("click", getSum);
